ba04f303be430b9fad3f9bbbaae0c5d8e8b88f65
Casperfab <casperfab@gmail.com>
2014-05-24 19:41:32 +0200

    Spawns

---
+++ Assets/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	private bool spawnRdy = true;
+	public GameObject[] burger;
+	public GameObject spawnPoint;
+	public float speed = 0.5f;
+	private int randomRange;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(spawnRdy){
+			randomRange = Random.Range(0, burger.Length-1);
+			spawnRdy = false;
+			GameObject tmpBurg = GameObject.Instantiate(burger[randomRange], spawnPoint.transform.position, Quaternion.identity) as GameObject;
+			//int randomRange = Random.Range(0, 360);
+			//tmpBurg.transform.rotation.z = randomRange;
+			StartCoroutine("ReadySpawn");
+		}
+		else
+			return;
+
+	}
+	IEnumerator ReadySpawn(){
+		if(speed >= 0.05)
+			speed -= (0.5f / 120);
+		yield return new WaitForSeconds (speed);
+		spawnRdy = true;
+	}
+}
