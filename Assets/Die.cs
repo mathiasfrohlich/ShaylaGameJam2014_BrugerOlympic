ec1d502e265f2a645bd81470f86f0e4d9921db7e
mkfs91 <Ramax@h58.aau1x.klient.sydhavn2.site.aau.dk>
2014-05-24 19:17:10 +0200

    spwner ref

--- Assets/Die.cs
+++ Assets/Die.cs
@@ -25,6 +25,8 @@ public class Die : MonoBehaviour {
 		if(other.tag == "Burger"){
 			Time.timeScale = 0.0f;
 			dead = true;
+
+			GameObject.FindGameObjectWithTag("Spawner").GetComponent<Spawner>().speed = 0;
 		}
 
 
@@ -33,8 +35,19 @@ public class Die : MonoBehaviour {
 	void OnGUI () {
 		if(dead){
 			GUI.Box(new Rect(Screen.width/2-200,Screen.height/2-100,400,200), " You were caught in the Act\n\n Off Picking up burgers off the floor\n\n\n You got " + burgers);
-		}
 
+			if (GUI.Button(new Rect(Screen.width/2-50, Screen.height/2+50, 100, 50), "Restart")){
+				foreach(GameObject g in GameObject.FindGameObjectsWithTag("Burger"))
+					Destroy(g);
+				dead = false;
+				Time.timeScale = 1.0f;
+				ameObject.FindGameObjectWithTag("Spawner").GetComponent<Spawner>().speed = 1;
+
+			}
+
+			
+		}
+		
 		GUI.Box(new Rect(10,10,200,30), "Burgers : "+burgers);
 
 
