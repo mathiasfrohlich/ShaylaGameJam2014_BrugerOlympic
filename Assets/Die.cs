351371d34d7f73df1ed5213848a62f4d5426287d
mkfs91 <Ramax@h58.aau1x.klient.sydhavn2.site.aau.dk>
2014-05-24 18:51:32 +0200

    line

---
+++ Assets/Die.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Die : MonoBehaviour {
+
+	public bool dead = false;
+
+	public int burgers = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		Time.timeScale = 0.0f;
+		dead = true;
+
+
+	}
+
+	void OnGUI () {
+		if(dead){
+			GUI.Box(new Rect(Screen.width/2-200,Screen.height/2-100,400,200), " You were caught in the Act\n\n Off Picking up burgers off the floor\n\n\n You got " + burgers);
+		}
+
+	}
+}
+
