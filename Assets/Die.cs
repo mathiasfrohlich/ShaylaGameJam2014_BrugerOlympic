930b91bbbf2158f11735eb5a4bb92c5672268972
mkfs91 <Ramax@h58.aau1x.klient.sydhavn2.site.aau.dk>
2014-05-24 19:08:48 +0200

    burger + death + score

--- Assets/Die.cs
+++ Assets/Die.cs
@@ -18,9 +18,14 @@ public class Die : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
+		print(other.gameObject.name);
 
-		Time.timeScale = 0.0f;
-		dead = true;
+
+
+		if(other.tag == "Burger"){
+			Time.timeScale = 0.0f;
+			dead = true;
+		}
 
 
 	}
@@ -29,6 +34,11 @@ public class Die : MonoBehaviour {
 		if(dead){
 			GUI.Box(new Rect(Screen.width/2-200,Screen.height/2-100,400,200), " You were caught in the Act\n\n Off Picking up burgers off the floor\n\n\n You got " + burgers);
 		}
+
+		GUI.Box(new Rect(10,10,200,30), "Burgers : "+burgers);
+
+
+
 
 	}
 }
---
+++ Assets/burger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class burger : MonoBehaviour {
+
+	public int amount = 1;
+
+	public int counter = 0 ;
+
+	// Use this for initialization
+	void Awake () {
+	
+
+	}
+
+	
+
+	// Update is called once per frame
+	void Update () {
+	
+		if(counter == amount){
+			GameObject.FindGameObjectWithTag("Finish").GetComponent<Die>().burgers ++;
+			Destroy(this.gameObject);
+		}
+
+
+	}
+}
